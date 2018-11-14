55eb2a2509fc551bd93ab70585c119aecf881a9d
DannyLiang <youchenliang@gmail.com>
2018-11-15 00:24:41 +0800

    [hw2]deploy smart contract with web3

--- hw2/deploy.js
+++ hw2/deploy.js
@@ -1,16 +1,26 @@
-const fs = require('fs')
-const Web3 = require('web3')
+const fs = require('fs');
+const Web3 = require('web3');
 
-let web3 = new Web3('http://localhost:8545')
+let web3 = new Web3('http://localhost:8545');
 
-const abi = JSON.parse(fs.readFileSync('./contract/Bank_sol_Bank.abi').toString())
-const bytecode = '0x' + fs.readFileSync('./contract/Bank_sol_Bank.bin').toString()
+const abi = JSON.parse(
+  fs.readFileSync('./contract/__hw2_Bank_sol_Bank.abi').toString(),
+);
+const bytecode =
+  '0x' + fs.readFileSync('./contract/__hw2_Bank_sol_Bank.bin').toString();
 
-let bank = new web3.eth.Contract(abi)
+let bank = new web3.eth.Contract(abi);
 
-web3.eth.getAccounts().then(function (accounts) {
-
-    // deploy contract
-    // your code
-
-})
+web3.eth.getAccounts().then(accounts => {
+  // deploy contract
+  // your code
+  bank
+    .deploy({
+      data: bytecode,
+    })
+    .send({
+      from: accounts[0],
+      gas: 1500000,
+    })
+    .on('receipt', receipt => console.log(receipt))
+});
